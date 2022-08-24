1387cdaac503e29be59bcf8f23ed9e8aed47fdeb
manon.alcuna <manon.alcuna@laposte.net>
2022-08-24 15:42:12 +0200

    Ready to start

--- app/src/main/java/com/manon/android/appbirthday/activities/SplashScreenActivity.java
+++ app/src/main/java/com/manon/android/appbirthday/activities/SplashScreenActivity.java
@@ -1,6 +1,19 @@
 package com.manon.android.appbirthday.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+/*        if ()
+        startActivity(new Intent(this, MainActivity.class));
+else*/
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
 }
